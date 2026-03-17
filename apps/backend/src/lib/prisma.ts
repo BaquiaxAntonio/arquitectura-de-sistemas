8117718d0845abd32264bc328dd535f850071721
BaquiaxAntonio <henrybaquiax@umes.edu.gt>
2026-03-16 23:10:32 -0600

    fix(prima.ts): mejora de prismas.ts

--- apps/backend/src/lib/prisma.ts
+++ apps/backend/src/lib/prisma.ts
@@ -1,12 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 
-// Usamos DATABASE_URL (pooler) para runtime, evita P1001 en Render
+// Usamos DATABASE_URL (pooler público) para runtime en Render
 const databaseUrl = process.env.DATABASE_URL;
-if (!databaseUrl) {
-  throw new Error(
-    "DATABASE_URL no está definida. Asegúrate de configurarla en Render."
-  );
-}
+if (!databaseUrl) throw new Error("DATABASE_URL no está definida");
 
 const prisma = new PrismaClient({
   datasources: { db: { url: databaseUrl } },
